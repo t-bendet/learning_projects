3968d1f107a21fd3ec52283ceade9ce988933710
t-bendet <talbendet21@gmail.com>
2021-06-26 20:02:35 +0300

    maps done

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -136895,6 +136895,10 @@ function () {
     };
   }
 
+  User.prototype.markerContent = function () {
+    return "User Name: " + this.name;
+  };
+
   return User;
 }();
 
@@ -136927,11 +136931,58 @@ function () {
     };
   }
 
+  Company.prototype.markerContent = function () {
+    return "\n      <div>\n        <h1>CompanyName: " + this.companyName + "</h1>\n        <h3>CatchPhrase: " + this.catchPhrase + "</h3>\n      <div>  \n    ";
+  };
+
   return Company;
 }();
 
 exports.Company = Company;
-},{"faker":"../../../../../node_modules/faker/index.js"}],"src/index.ts":[function(require,module,exports) {
+},{"faker":"../../../../../node_modules/faker/index.js"}],"src/CustomMap.ts":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.CustomMap = void 0;
+
+var CustomMap =
+/** @class */
+function () {
+  function CustomMap(divId) {
+    this.googleMap = new google.maps.Map(document.querySelector(divId), {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0
+      }
+    });
+  }
+
+  CustomMap.prototype.addMarker = function (mappable) {
+    var _this = this;
+
+    var marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng
+      }
+    });
+    marker.addListener("click", function () {
+      var infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent()
+      });
+      infoWindow.open(_this.googleMap, marker);
+    });
+  };
+
+  return CustomMap;
+}();
+
+exports.CustomMap = CustomMap;
+},{}],"src/index.ts":[function(require,module,exports) {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
@@ -136942,11 +136993,14 @@ var User_1 = require("./User");
 
 var Company_1 = require("./Company");
 
+var CustomMap_1 = require("./CustomMap");
+
 var user = new User_1.User();
 var company = new Company_1.Company();
-console.log(user);
-console.log(company);
-},{"./User":"src/User.ts","./Company":"src/Company.ts"}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var customMap = new CustomMap_1.CustomMap("#map");
+customMap.addMarker(user);
+customMap.addMarker(company);
+},{"./User":"src/User.ts","./Company":"src/Company.ts","./CustomMap":"src/CustomMap.ts"}],"../../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -136974,7 +137028,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57613" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50895" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,16 +1,20 @@
-import faker from 'faker' 
+import faker from "faker";
+import { Mappable } from "./CustomMap";
 
-export class User {
-  name:string;
-  location:{
-    lat:number;
-    lng:number;
-  }
-  constructor(){
-    this.name = faker.name.findName()
+export class User implements Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  constructor() {
+    this.name = faker.name.findName();
     this.location = {
-      lat:parseFloat(faker.address.latitude()),
-      lng:parseFloat(faker.address.longitude())
-    }
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+  markerContent(): string {
+    return `User Name: ${this.name}`;
   }
 }
